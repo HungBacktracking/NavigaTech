e370ac271bfb48346546f629cb2267b59706d1f3
HungBacktracking <hoanghung1182003@gmail.com>
2025-04-28 15:25:56 +0700

    add validator handler

--- backend/app/exceptions/exception_handlers.py
+++ backend/app/exceptions/exception_handlers.py
@@ -2,6 +2,7 @@ from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse
 from app.exceptions.custom_error import CustomError
 from app.exceptions.errors.CustomClientException import ClientException
+from fastapi.exceptions import RequestValidationError
 from app.schema.error import ErrorResponse
 
 
@@ -12,6 +13,15 @@ def register_exception_handlers(app: FastAPI):
         payload = ErrorResponse(message=exc.detail, code=exc.code)
 
         return JSONResponse(status_code=exc.status_code, content=payload.model_dump())
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+        first_err = exc.errors()[0]
+        code = "50"
+        message = f"{first_err['loc'][-1]}: {first_err['msg']}"
+        payload = ErrorResponse(code=code, message=message)
+
+        return JSONResponse(status_code=400, content=payload.model_dump())
 
     @app.exception_handler(Exception)
     async def generic_exception_handler(request: Request, exc: Exception):
