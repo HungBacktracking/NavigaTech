269209cbf71c72d3d02a78f041889bc9194829e3
Lê Đức Tài <87483288+NBTailee@users.noreply.github.com>
2025-05-12 17:41:54 +0700

    update(small talk component): Update small_talk_check.py

--- AI_modules/rag_modules/small_talk_check.py
+++ AI_modules/rag_modules/small_talk_check.py
@@ -3,16 +3,26 @@ from typing import List
 
 
 class AdvancedRuleBasedSmallTalkChecker:
-    def __init__(self, threshold: int = 3):
+    def __init__(self, threshold: int = 4):
         self.threshold = threshold
         self.rule_patterns: List[(re.Pattern, int)] = [
             (re.compile(r'\b(hi|hello|hey|good (morning|afternoon|evening))\b', re.I), 3),
             (re.compile(r'\b(bye|goodbye|see you|farewell)\b', re.I), 3),
             (re.compile(r'\b(how are you|how\'s it going|what\'s up)\b', re.I), 3),
             (re.compile(r'\b(joke|funny|laugh)\b', re.I), 4),
-            (re.compile(r'\b(thanks?|thank you|appreciate)\b', re.I), 2),
             (re.compile(r'\b(weather|time|weekend|holiday)\b', re.I), 1),
-            (re.compile(r"\b(i('m| am)|my name is|this is|call me)\b", re.I), 3),
+            (re.compile(r'\b(are you real|what are you|who are you|can you think)\b', re.I), 4),
+            (re.compile(r'\b(how are you|how\'s it going|what\'s up|how do you feel)\b', re.I), 3),
+            (re.compile(r'\b(i\'m (bored|tired|happy|sad|excited|angry|upset))\b', re.I), 4),
+            (re.compile(r'\b(joke|funny|laugh|make me laugh|tell me a joke)\b', re.I), 4),
+            (re.compile(r'\b(thanks?|thank you|appreciate|you\'re the best)\b', re.I), 2),
+            (re.compile(r'\b(weather|time|what time is it|is it hot outside)\b', re.I), 1),
+            (re.compile(r'\b(not much|just chilling|hanging out|what are you up to)\b', re.I), 2),
+            (re.compile(r'\b(do you think|am i (smart|okay|nice)|what do you think)\b', re.I), 3),
+            (re.compile(r'\b(favorite (color|food|movie|song)|what do you like)\b', re.I), 3),
+            (re.compile(r"\b(i('m| am)|my name is|this is|call me|who am i)\b", re.I), 3),
+            (re.compile(r'\b(you\'re (awesome|amazing|cool|funny|smart))\b', re.I), 3),
+            (re.compile(r'\b(i feel (happy|sad|angry|confused|excited))\b', re.I), 4),
         ]
 
         self.fallback_classifier = None
