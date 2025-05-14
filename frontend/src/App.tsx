508227cf650aea016fe3ee7310e878d9512b44c0
anhthais <anhthai733029@gmail.com>
2025-05-14 12:36:26 +0700

    fix: unexpected unresolved merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import AppRouter from './routes';
 import { Suspense } from 'react';
 import queryClient from './lib/clients/query-client';
 import AuthProvider from './contexts/auth/auth-provider';
 import FullscreenLoader from './components/fullscreen-loader';
-import NotificationsProvider from './components/notifications';
 import './App.css';
 
 function App() {
@@ -13,9 +11,7 @@ function App() {
     <Suspense fallback={<FullscreenLoader />}>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
-          <NotificationsProvider>
-            <AppRouter />
-          </NotificationsProvider>
+          <AppRouter />
         </AuthProvider>
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
